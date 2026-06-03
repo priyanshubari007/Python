96228db3d3f34950023882c22cafec47261779c5
priyanshubari007 <priyanshu.bari007@gmail.com>
2026-06-03 22:54:52 +0530

    Add files via upload

---
+++ My Introduction.py
@@ -0,0 +1,15 @@
+print("My Name Is Priyanshu Bari")
+
+print("I Study In Grade 7")
+
+print("I Am In Future Campus School,Sonarpur")
+
+print("I Live In Harinavi,Rajpur Sonarpur")
+
+print("My Hobbies are playing chess,drawing,listening to music")
+
+print("My Email Id is-priyanshubari007@gmail.com")
+
+print("The City I live in is-Kolkata and the State is-West Bengal")
+
+print("That is all about my introduction")
