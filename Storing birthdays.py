1d0ca828eb59f78a1e0e4045d186893f86080b57
priyanshubari007 <priyanshu.bari007@gmail.com>
2026-06-06 21:16:13 +0530

    Add files via upload

---
+++ Storing birthdays.py
@@ -0,0 +1,39 @@
+name1 = "Priyanshu"
+birthday1 = "10-03-2014"
+
+name2 = "Priya"
+birthday2 = "22-03-2001"
+
+name3 = "Rahul"
+birthday3 = "10-07-2002"
+
+name4 = "Sneha"
+birthday4 = "05-11-2003"
+
+name5 = "Arjun"
+birthday5 = "18-12-2004"
+
+print("Birthday Records")
+print("------------------")
+
+print("Name:",name1)
+print("Birthday:",birthday1)
+print()
+
+print("Name:",name2)
+print("Birthday:",birthday2)
+print()
+
+print("Name:",name3)
+print("Birthday:",birthday3)
+print()
+
+print("Name:",name4)
+print("Birthday:",birthday4)
+print()
+
+print("Name:",name5)
+print("Birthday:",birthday5)
+print()
+
+print("Total birthday records:5")
