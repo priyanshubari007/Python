fd3a3eab26a9918714db6eccc4ab80f4571c8d30
priyanshubari007 <priyanshu.bari007@gmail.com>
2026-06-06 21:03:35 +0530

    Add files via upload

---
+++ Understanding Variables.py
@@ -0,0 +1,16 @@
+# Creating different variables
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+codingal= "codingal"
+
+print(codingal)
+
+# User Input
+name = input("Enter your name: ")
+
+print("\nHello", name, "\nWelcome to Codingal")
