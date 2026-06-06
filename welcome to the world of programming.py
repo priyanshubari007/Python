91d7fefcc49379c24f0a2a772048fc37ed143667
priyanshubari007 <priyanshu.bari007@gmail.com>
2026-06-06 20:56:15 +0530

    Add files via upload

---
+++ welcome to the world of programming.py
@@ -0,0 +1,15 @@
+#Print a sentence
+print("Welcome to the world of programming!")
+
+#Print a number
+
+print(4)
+
+#Next line after print
+print("hello Python \n")
+
+#Pass multiple arguments for printing
+print("hello",5)
+
+#End argument of print statement
+print("welcome to ", end= "*")
